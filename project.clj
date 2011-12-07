5bbeb76fd98129516de8c0b2f4a2cf6f9b0779bf
takuma maruyama <maruyama.ylab@gmail.com>
2011-12-07 22:42:33 +0900

    io and project changed

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
 								 [org.clojure/clojure-contrib "1.2.0"]
                  [tw.edu.ntu.csie/libsvm "3.1"]]
-	:main cl-svm.core)
+	:dev-dependencies [[org.clojars.weavejester/autodoc "0.9.0"]]
+	:main cl-svm.bow
+	:autodoc { :name "cl-svm", :page-title "cl-svm API Documentation"})
 
--- src/ma3/io.clj
+++ src/ma3/io.clj
@@ -4,27 +4,66 @@
              InputStream FileInputStream
 						 OutputStream FileOutputStream
 						 BufferedWriter OutputStreamWriter
-             BufferedReader InputStreamReader))
-	)
+             BufferedReader InputStreamReader)))
 
 ;; file
 ;;----------------
-(defn delete-file [#^String f]
+(defn delete-file
+  "delete file"
+  [#^String f]
 	(.delete (File. f)))
 
-(defn file-extension [#^String f]
+(defn file-extension 
+  "get file extension"
+  [#^String f]
 	(if-let [match (re-seq #"\.(\w+)$" f)]
 		(nth (first match) 1)))
 
+(defn file-head
+  "get file head"
+  [#^String f]
+  (if-let [match (re-seq #"([^\/]+)\.\w+$" f)]
+    (nth (first match) 1)))
+
+(defn file-list
+  "get files if match"
+  [#^String dirpath & [{:keys [type]}]]
+  (let [flist (if-let [d (File. dirpath)] (.listFiles d) nil)
+        typefn (if type
+                  (fn [file]
+                    (if (and
+                          (not= file nil)
+                          (= type (file-extension (.getName file))))
+                        file))
+                  identity)
+        filefn (fn [file]
+                  (if (.isFile file) file nil))]
+    (if flist
+      (map #(.getPath %) (filter (comp typefn filefn) flist)))))
+
+(declare str2file lazy-input-line tois)
+
+(defn file-join
+  "file join"
+  [dstfile & files]
+  (str2file dstfile (apply str (flatten (map #(lazy-input-line (tois %)) files)))))
+
 ;; stream
 ;;----------------
-(defn tois #^InputStream [#^String f]
-  "String to InputStream"
+(defn tois
+  "input inputStream"
+  #^InputStream [#^String f]
   (FileInputStream. (File. f)))
 
 (defn toos #^OutputStream [#^String f]
 	"String to OutputStream"
 	(FileOutputStream. (File. f)))
+
+(defn tobw
+  "String to buffered writer"
+  #^BufferedWriter
+  [#^String f]
+  (BufferedWriter. (OutputStreamWriter. (toos f))))
 
 (defn lazy-input [#^InputStream input-stream]
 	"lazy sequence for input character"
@@ -56,4 +95,11 @@
 					(when-not (== readed -1)
 						(.write os data 0 readed)
 						(recur)))))))
+
+(defn str2file
+  "write string to file"
+  [#^String filepath #^String contents]
+  (with-open [bw (tobw filepath)]
+    (.write bw contents 0 (.length contents))))
+
 
